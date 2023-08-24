93e207a34c96232c2a5f01de6bd493123f945895
Guilherme <guilherme.beutler@outlook.com>
2023-08-24 10:23:18 -0300

    refactor(supplier, client): fixed the interfaces

--- src/client/interfaces/client.interface.ts
+++ src/client/interfaces/client.interface.ts
@@ -1,11 +1,21 @@
+export default interface IClientAddress {
+	street: string;
+	number: string;
+	city: string;
+	state: string;
+	zipCode: string;
+}
+
 export default interface IClient {
 	id: number;
 	name: string;
 	email: string;
 	password: string;
 	phone: string;
-	address: string;
-	city: string;
-	state: string;
-	zipCode: string;
+	address: IClientAddress;
+	cpf?: string;
+	cnpj?: string;
+	createdAt: Date;
+	updatedAt: Date;
+	appointments: string[];
 }
--- src/supplier/interfaces/supplier.interface.ts
+++ src/supplier/interfaces/supplier.interface.ts
@@ -1,1 +1,22 @@
-export default interface ISupplier {}
+export default interface ISupplierAddress {
+	street: string;
+	number: string;
+	city: string;
+	state: string;
+	zipCode: string;
+}
+
+export default interface ISupplier {
+	id: string;
+	name: string;
+	email: string;
+	password: string;
+	phone: string;
+	address: ISupplierAddress;
+	cpf?: string;
+	cnpj?: string;
+	createdAt: Date;
+	updatedAt: Date;
+	services: string[];
+	appointments: string[];
+}
