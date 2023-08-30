1bf0e2b6a578b783269fbd0223d851d02a024ab1
Guilherme <guilherme.beutler@outlook.com>
2023-08-29 21:36:24 -0300

    feat(auth): implement currentUser

---
+++ src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { AuthRequest } from '../models/AuthRequest';
+import { IClient } from '@client/interfaces';
+import { ISupplier } from '@supplier/interfaces';
+
+export const CurrentUser = createParamDecorator(
+	(data: unknown, context: ExecutionContext): IClient | ISupplier => {
+		const request = context.switchToHttp().getRequest<AuthRequest>();
+
+		return request.user;
+	},
+);
